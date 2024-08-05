b925cd6972cd87e7e2e7de9a64a3abc0c34872de
Yemyam <ethan.braum@gmail.com>
2024-08-05 22:36:43 +0800

    change grid to use flexbox

--- js-script.js
+++ js-script.js
@@ -1,9 +1,12 @@
 const container = document.querySelector("#container");
 // loop to add grid cells into container
 for (let i = 0; i < 16; i++){
+    var gridRow = document.createElement("div")
+    gridRow.classList.add("row")
     for (let j = 0; j < 16; j++){
         var cell = document.createElement("div");
         cell.classList.add("cell")
-        container.appendChild(cell);
+        gridRow.appendChild(cell);
     }
+    container.appendChild(gridRow)
 }
