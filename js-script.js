0d9ce9329148f8bf98c269162998cc1a2ed4c3b8
Yemyam <ethan.braum@gmail.com>
2024-08-06 12:42:12 +0800

    add hover effect

--- js-script.js
+++ js-script.js
@@ -1,21 +1,34 @@
 const container = document.querySelector("#container");
+var dimension = 25;
 // loop to add grid cells into container
-for (let i = 0; i < 16; i++){
-    var gridRow = document.createElement("div")
-    gridRow.classList.add("row")
-    for (let j = 0; j < 16; j++){
+for (let i = 0; i < dimension; i++){
+    for (let j = 0; j < dimension; j++){
         var cell = document.createElement("div");
+        let boxSize = 600/dimension;
         cell.classList.add("cell")
-        gridRow.appendChild(cell);
+        cell.style.maxWidth = boxSize + 'px';
+        cell.style.maxHeight = boxSize + 'px';
+        cell.style.minWidth = boxSize + 'px';
+        cell.style.minHeight = boxSize + 'px';
+        container.appendChild(cell);    
     }
-    container.appendChild(gridRow)
 }
 const cells = document.querySelectorAll(".cell");
 cells.forEach((cell) => {
     cell.addEventListener("mouseenter", () => {
-        cell.setAttribute("style","background-color:#cfcfe5;")
+        cell.classList.add("hovered")
     })
     cell.addEventListener("mouseleave", () => {
-        cell.setAttribute("style","background-color: none;")
+        cell.classList.remove("hovered")
     })
 })
+
+// container.addEventListener("mouseover",(event) => {
+//     let target = event.target;
+//     target.classList.add("hovered");
+// })
+// container.addEventListener("mouseleave", (event => {
+//     let target = event.target;
+//     console.log(target)
+//     target.classList.remove("hovered")
+// }))
